152b754c61531cfeeeeca852a716fc97fb018268
Shaghayegh Asadi <shaghayegh.asadi69@yahoo.com>
2023-08-02 16:28:05 +0100

    initial

---
+++ server/models/Movie.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const movieSchema = new Schema({
+  name: String,
+  img_url: String,
+  director: String,
+  year: Number,
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+// movies
+
+module.exports = Movie;
