2b6eb9c1503cd0cbcbc18041d19521a8e43468f8
Shaghayegh Asadi <shaghayegh.asadi69@yahoo.com>
2023-08-02 19:28:08 +0100

    new

---
+++ client/src/components/main.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import MovieCard from "./MovieCard/MovieCard";
+import Form from "./Form/Form";
+
+export default function Main() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  // READ
+  async function getMovies() {
+    let API = "http://localhost:4242/movies";
+    const result = await axios.get(API);
+    console.log(result.data);
+    setMovies(result.data);
+  }
+
+  // CREATE
+  const handleAddMovie = async (newMovieFormData) => {
+    const res = await axios.post(
+      "http://localhost:4242/movies",
+      newMovieFormData
+    );
+    setMovies([...movies, res.data]);
+  };
+
+  // delete Movie
+  const handleDelete = async (id) => {
+    console.log("clicked");
+    const res = await axios.delete(`http://localhost:4242/movies/${id}`);
+    console.log(res);
+    getMovies();
+  };
+
+  // UPDATE
+
+  const handleUpdateMovie = async (movie) => {
+    await axios.put(`http://localhost:4242/movies/${movie._id}`, movie);
+    getMovies();
+  };
+
+  // talk about HTTP Request structure ->
+
+  return (
+    <div>
+      <Form onSubmitFunc={handleAddMovie} />
+      <h3>This is my Main!</h3>
+      <MovieCard
+        movies={movies}
+        handleDelete={handleDelete}
+        handleUpdateMovie={handleUpdateMovie}
+      />
+    </div>
+  );
+}
---
+++ client/src/components/movieCard/moviCard.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./MovieCards.css";
+import Form from "../Form/Form";
+
+export default function MovieCard({ movies, handleDelete, handleUpdateMovie }) {
+  return (
+    <div>
+      {movies.map((movie) => {
+        return (
+          <div key={movie._id}>
+            <div>
+              {/* we're using two functions here -> anon callback, that callback invokes the handle delete. We're passing the movie id as our arugment to the handleDelete function */}
+              <p onClick={() => handleDelete(movie._id)}>X</p>
+              <h3>{movie.name}</h3>
+              <img src="{movie.img_url} "/>
+              <p>
+                {movie.director}, {movie.year}
+              </p>
+            </div>
+            <Form onSubmitFunc={handleUpdateMovie} movie={movie} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
